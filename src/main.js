97cdaa865f6d110950825b2fc0dfc2ea7b16b442
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-10 23:17:10 +0300

    update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,48 @@ import { fetchImageByQuery } from "./js/pixabay-api";
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-
+import iconError from './img/icon-error.svg';
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const searchForm = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+
+const searchFormSubmit = event => {
+    event.preventDefault();
+
+    const searchQuery = event.target.elements.user_image.value;
+
+fetchImageByQuery(searchQuery)
+.then(data => {
+    const imageTemplate = data.results
+    .map(image => createGalleryCard(image))
+        .join('');
+
+        gallery.innerHTML(imageTemplate);
+})
+.catch(error => {
+    console.log(error);
+});
+};
+
+searchForm.addEventListener('submit', searchFormSubmit);
+
+// const checkEmptyInput = searchQuery => {
+//     if (searchQuery.trim() === '') {
+
+        
+//     }
+// }
+
+
+// iziToast.error({
+// title: 'Error',
+// message: `❌ Rejected promise in ${delay}ms`,
+// backgroundColor: '#ef4040',
+// theme: 'dark',
+// color: 'red',
+// iconUrl: iconError,
+// position: 'topRight',
+// }); 
