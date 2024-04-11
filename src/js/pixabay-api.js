f50692160cf7a0837738061cc87007ea8f1588c1
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-11 07:50:01 +0300

    add iziToast

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
 // У файлі pixabay-api.js зберігай функції для HTTP-запитів.
 
 
-export const fetchImageByQuery = query => {
-    return fetch(`https://pixabay.com/api/?key=43329687-8aa5e523ea6ec5a66d8459b66&query=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
+export function fetchImageByQuery(query) {
+return fetch(`https://pixabay.com/api/?key=43329687-8aa5e523ea6ec5a66d8459b66&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
 .then(response => {
     if(!response.ok){
         throw new Error(response.status);
     }
     return response.json();
 });
-};
+}
--- src/main.js
+++ src/main.js
@@ -11,42 +11,50 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const searchForm = document.querySelector('.search-form');
+
 const gallery = document.querySelector('.gallery');
 
 const searchFormSubmit = event => {
     event.preventDefault();
-
     const searchQuery = event.target.elements.user_image.value;
+    if (searchQuery === '') {
+        alert('Please enter a search query');
+        return;
+    }
 
-
+  
 fetchImageByQuery(searchQuery)
 .then(data => {
-    const imageTemplate = data.map(image => createGalleryCard(image))
-        const stringImage = imageTemplate.join('');
+    if (data.hits.length === 0) {
+        iziToast.error({
+                title: 'Error',
+                message: "Sorry, there are no images matching your search query. Please try again!",
+                backgroundColor: '#ef4040',
+                theme: 'dark',
+                color: 'red',
+                iconUrl: iconError,            position: 'topRight',
         
-        gallery.innerHTML(stringImage);
+        });
+    }
+    else{
+    const imageTemplate = data.hits.map(image => createGalleryCard(image)).join('');
+        gallery.innerHTML = imageTemplate;}
 })
 .catch(error => {
-    console.log(error);
+    iziToast.error({
+        title: 'Error',
+        message: "Sorry, there are no images matching your search query. Please try again!",
+        backgroundColor: '#ef4040',
+        theme: 'dark',
+        color: 'red',
+        iconUrl: iconError,     
+       position: 'topRight',
+})
+        console.log(error);
 });
 };
 
 searchForm.addEventListener('submit', searchFormSubmit);
 
-// const checkEmptyInput = searchQuery => {
-//     if (searchQuery.trim() === '') {
-
-        
-//     }
-// }
 
 
-// iziToast.error({
-// title: 'Error',
-// message: `❌ Rejected promise in ${delay}ms`,
-// backgroundColor: '#ef4040',
-// theme: 'dark',
-// color: 'red',
-// iconUrl: iconError,
-// position: 'topRight',
-// }); 
